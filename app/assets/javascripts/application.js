5ac27852aedf2cd247f9b43f32c8d316d493751b
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-24 21:59:19 +0100

    Add auto-scrolling to chat window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,17 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+}
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown()
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 })
 
