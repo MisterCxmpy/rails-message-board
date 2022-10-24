ac9407123bdadf688fba7fb9b29de07dc9375946
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-24 17:34:50 +0100

    Build message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
+    has_many :messages
     has_secure_password
 end
