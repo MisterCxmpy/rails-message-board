11ed90492d24c6bc50e8442ef69760a8a8d9cb8b
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-24 22:37:37 +0100

    Bug fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true
-  scope :custom_display, => { order(:created_at).last(20) }
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
