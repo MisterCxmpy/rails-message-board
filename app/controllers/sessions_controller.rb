d4985ab60b8cfaee9b64b002307038c02199012d
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-10-24 21:03:42 +0100

    Add authentication system - create and destory sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,22 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Incorrect username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
 end
